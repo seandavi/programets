59ca0d94ea5876634aac9fcbb7754b78d3448e2d
David Mayer <david.mayer@cuanschutz.edu>
2025-05-12 08:20:18 -0600

    add roxygen

--- R/nih_report.R
+++ R/nih_report.R
@@ -1,8 +1,24 @@
-library(httr2)
-library(dplyr)
-library(purrr)
-library(tibble)
-
+#' Get Publications for Core Projects
+#'
+#' This function retrieves a list of publications associated with a set of core
+#' project numbers from the NIH RePORTER API.
+#'
+#' @param core_project_numbers A character vector of core project numbers.
+#'
+#' @importFrom dplyr arrange bind_rows mutate tibble
+#' @importFrom purrr map_dfr
+#' @importFrom httr2 req_body_json req_headers req_method req_perform resp_body_json
+#' 
+#' @return A tibble with the following columns:
+#' \describe{
+#'   \item{core_project_number}{The core project number.}
+#'   \item{found}{A logical indicating whether any publications were found for this core project number.}
+#'   \item{applid}{The application ID associated with the publication.}
+#'   \item{pmid}{The PubMed ID associated with the publication.}
+#'   \item{pubmed_url}{The URL of the PubMed page for the publication.}
+#' }
+#' 
+#' @export
 get_publications_for_core_projects <- function(core_project_numbers) {
   req <- request("https://api.reporter.nih.gov/v2/publications/search") |>
     req_method("POST") |>
