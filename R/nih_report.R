29f99a05d481224791a9273eb8f31c75a08fe3b9
David Mayer <david.mayer@cuanschutz.edu>
2025-05-12 08:06:11 -0600

    draft core project number to pmid - input list of core project numbers - query NIH RePORTER api - parse resutls, create pubmed_url - add all user input values, even if not found - TODO: roxygenize, imports

---
+++ R/nih_report.R
@@ -0,0 +1,47 @@
+library(httr2)
+library(dplyr)
+library(purrr)
+library(tibble)
+
+get_publications_for_core_projects <- function(core_project_numbers) {
+  req <- request("https://api.reporter.nih.gov/v2/publications/search") |>
+    req_method("POST") |>
+    req_headers("Content-Type" = "application/json") |>
+    req_body_json(list(
+      criteria = list(core_project_nums = core_project_numbers),
+      include_fields = list( "applid", "coreproject", "pmid")
+    )) 
+  
+  resp <- req |>
+    req_perform() |>
+    resp_body_json()
+
+  # Parse returned publications to a tibble
+  results_tbl <- 
+    purrr::map_dfr(resp$results, function(pub) {
+      tibble(
+        core_project_number = pub$coreproject,
+        found = TRUE,
+        applid = as.character(pub$applid),
+        pmid = as.character(pub$pmid),
+        pubmed_url = paste0("https://pubmed.ncbi.nlm.nih.gov/", pub$pmid)
+      )
+    }
+  )
+
+  # Ensure all user input core project numbers are represented
+  all_results <- 
+    dplyr::bind_rows(
+      results_tbl,
+      tibble(core_project_number = setdiff(core_project_numbers, results_tbl$core_project_number)) |>
+      mutate(
+        found = FALSE,
+        applid = NA_character_,
+        pmid = NA_character_,
+        pubmed_url = NA_character_
+      )
+    ) |>
+    arrange(core_project_number)
+
+  return(all_results)
+}
